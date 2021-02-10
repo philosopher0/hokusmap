0ab4617efa89246560b0d8350ab121f66d9186e5
Philipp Kastner <philippkastner@outlook.com>
2021-02-10 14:07:53 +0100

    logotileset

---
+++ test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="test" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="../../../Hokus_193x200.png" width="193" height="201"/>
+</tileset>
